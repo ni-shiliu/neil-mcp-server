ef1c7ab4a210058fcf955d9c6ffd629ee55b2a35
nihao <nihao@za-doctor.com>
2025-04-08 10:58:58 +0800

    build(project): init

---
+++ mcp-mysql-server/src/test/java/com/neil/mcp/server/mysql/McpMysqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neil.mcp.server.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpMysqlServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
